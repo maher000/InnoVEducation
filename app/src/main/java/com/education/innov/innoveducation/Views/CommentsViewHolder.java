e8b43f6da67bbbef9288b9e106760afc90e8effa
SyrineDridi <zarra.dbr@gmail.com>
2017-04-11 19:26:58 +0200

    first commit

--- app/src/main/java/com/education/innov/innoveducation/Views/CommentsViewHolder.java
+++ app/src/main/java/com/education/innov/innoveducation/Views/CommentsViewHolder.java
@@ -2,17 +2,29 @@ package com.education.innov.innoveducation.Views;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.education.innov.innoveducation.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Syrine on 07/04/2017.
  */
 
 public class CommentsViewHolder extends RecyclerView.ViewHolder {
-
+CircleImageView image_profile ;
+    TextView tvFullNameComment , tvDateComment , tvObjectComment ;
 
 
     public CommentsViewHolder(View view) {
         super(view);
+
+        tvFullNameComment = (TextView) view.findViewById(R.id.tvFullNameComment);
+        tvDateComment = (TextView) view.findViewById(R.id.tvDateComment);
+        tvObjectComment = (TextView) view.findViewById(R.id.tvObjectComment);
+
+
 
     }
 
--- app/src/main/java/com/education/innov/innoveducation/Views/CourseViewHolder.java
+++ app/src/main/java/com/education/innov/innoveducation/Views/CourseViewHolder.java
@@ -2,6 +2,11 @@ package com.education.innov.innoveducation.Views;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.education.innov.innoveducation.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Syrine on 05/04/2017.
@@ -9,9 +14,17 @@ import android.view.View;
 public class CourseViewHolder  extends RecyclerView.ViewHolder {
 
 
+CircleImageView image_profile_courses;
+    TextView tvFullNameCourses , tvMatiereCourses , dateCoursses ;
 
     public CourseViewHolder(View view) {
         super(view);
+        image_profile_courses = (CircleImageView) view.findViewById(R.id.image_profile_courses) ;
+        tvFullNameCourses = (TextView) view.findViewById(R.id.tvFullNameCourses);
+        tvMatiereCourses = (TextView) view.findViewById(R.id.tvMatiereCourses);
+        dateCoursses = (TextView) view.findViewById(R.id.tvdateCoursses);
+
+
 
     }
 
